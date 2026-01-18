cc3383a16399c7d2a433c806dc6b03858b22944e
jozanutta-beep <jozanutta@gmail.com>
2026-01-18 18:28:58 +0100

    Update script.js

--- script.js
+++ script.js
@@ -206,4 +206,3 @@ document.getElementById("resetSeasonBtn").onclick = async () => {
 document.getElementById("sortPointsBtn").onclick = () => sortSkatersBy("points");
 document.getElementById("sortGoalsBtn").onclick = () => sortSkatersBy("goals");
 document.getElementById("sortAssistsBtn").onclick = () => sortSkatersBy("assists");
-};
